467f01b56b19781df14b92042df82b2f99ad7a5c
Jared Galloway <jaredgalloway07@gmail.com>
2019-07-31 15:36:34 -0700

    updated script

--- P1.py
+++ P1.py
@@ -3,16 +3,37 @@ import argparse
 from collections import defaultdict
 from matplotlib import pyplot as plt
 import sys
+import gzip
+import numpy as np
 
-parser = argparse.ArgumentParser(description='This file is python code for part one of the demultiplexing \
-    assignment. This will read through a fastQ file, and get a distribution of quality scores for each bp position 
+parser = argparse.ArgumentParser(description='This file is python code for \
+    part one of the demultiplexing \
+    assignment. This will read through \
+    a fastQ file, and get a distribution \
+    of quality scores for each bp position \
     in every read.')
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-f', type=str, help='files to parse')
-parser.add_argument('-mbp', type=str, help='where to output buckets')
+parser.add_argument('-f', type=str, help='fastq files to parse')
+parser.add_argument('-nbp', type=int, help='number of base pairs in each read (or max)')
 
 args = parser.parse_args()
 
+def convert_phred(letter):
+    """Converts a single character into a phred score"""
+    return ord(letter) - 33
 
+quality_score_sums = np.zeros(args.nbp)
+num_records = 0
+with gzip.open(args.f, 'rt') as fqfp:
+    for LN,line in enumerate(fqfp):
+        if(LN%4==3):
+            num_records += 1 
+            for nt,score in enumerate(line.strip()):
+                quality_score_sums[nt] += convert_phred(score)
+
+# mean quality scores for ech position acrtoss all reads
+#mean_quality_scores = [qsum/num_records for qsum in quality_score_sums]
+outfile = open(args.f + "_mps.tsv","w")
+outfile.write("\t".join([str(x/num_records) for x in quality_score_sums]))
 
