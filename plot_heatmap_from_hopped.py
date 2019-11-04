0d4912e5b37c2840d5ff32cbf876e605d0508946
jgallowa07 <jaredgalloway07@gmail.com>
2019-11-03 19:20:18 -0800

    plotting heatmap

--- plot_heatmap_from_hopped.py
+++ plot_heatmap_from_hopped.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 import sys
 import gzip
 import numpy as np
+from helpers import *
 
 parser = argparse.ArgumentParser(description='This file is python code for \
     part one of the demultiplexing \
@@ -14,5 +15,36 @@ parser = argparse.ArgumentParser(description='This file is python code for \
     in every read.')
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-f', type=str, help='fastq files to parse')
-parser.add_argument('-nbp', type=int, help='number of base pairs in each read (or max)')
+parser.add_argument('-fq', type=str, help='fastq files to parse')
+parser.add_argument('-bar', type=str, help='valid barcodes')
+args = parser.parse_args()
+
+barcode_index = {} 
+indices = open(args.bar,"r")
+header = indices.readline()
+for index,barcode in enumerate(indices):
+    barcode = barcode.strip().split()[4]
+    barcode_index[barcode] = index
+
+
+#print(len(barcode_index)) 
+
+hopped_file_iterator = fastq_records_iterator(open(args.fq,"r"))
+hopped_matrix = np.zeros([len(barcode_index),len(barcode_index)])
+for i,record in enumerate(hopped_file_iterator):
+    header = record[0].strip().split("_")
+    bar1 = header[-2]
+    bar2 = header[-1]
+    
+    bar2_rc = reverse_compliment(header[-1])
+    print(bar2, bar2_rc)
+    break
+    hopped_matrix[barcode_index[bar2],barcode_index[bar1]] += 1
+
+#plt.imshow(hopped_matrix)
+#plt.show()
+     
+
+
+
+
